b555034f28dafc9df3fca21bbf8eaa0dd93b6436
Lightnindrummer <dersu13@yahoo.com>
2023-06-12 06:06:36 -0400

    finished

--- 2000sWebsite/index.js
+++ 2000sWebsite/index.js
@@ -9,16 +9,29 @@ navToggler.addEventListener("click", function () {
     nav.setAttribute("visible", true);
     navToggler.children[0].classList.add("fa-rotate-180");
   }
-}); 
-document.getElementById("tableOfContents").setAttribute("visible", false)
+});
+document.getElementById("tableOfContents").setAttribute("visible", false);
 document.getElementById("tableToggler").addEventListener("click", function () {
-  if (document.getElementById("tableOfContents").getAttribute("visible") === "true") { 
-    document.getElementById("tableOfContents").setAttribute("visible", false) 
-    document.getElementById("tableToggler").children[0].classList.add("fa-rotate-270") 
-    document.getElementById("tableToggler").children[0].classList.remove("fa-rotate-90")
-  } else { 
-    document.getElementById("tableOfContents").setAttribute("visible", true)  
-    document.getElementById("tableToggler").children[0].classList.add("fa-rotate-90")
-    document.getElementById("tableToggler").children[0].classList.remove("fa-rotate-270")
+  if (document.getElementById("tableOfContents").getAttribute("visible") === "true") {
+    document.getElementById("tableOfContents").setAttribute("visible", false);
+    document.getElementById("tableToggler").children[0].classList.add("fa-rotate-270");
+    document.getElementById("tableToggler").children[0].classList.remove("fa-rotate-90");
+  } else {
+    document.getElementById("tableOfContents").setAttribute("visible", true);
+    document.getElementById("tableToggler").children[0].classList.add("fa-rotate-90");
+    document.getElementById("tableToggler").children[0].classList.remove("fa-rotate-270");
   }
+});
+//sorry was in a rush
+document.getElementById("htmlContentToggler").addEventListener("click", function () {
+  document.getElementById("htmlContent").style.display = "block"; 
+  document.getElementById("htmlContentToggler").innerText = "HTML"
+});
+document.getElementById("cssContentToggler").addEventListener("click", function () {
+  document.getElementById("cssContent").style.display = "block"; 
+  document.getElementById("cssContentToggler").innerText = "CSS"
+});
+document.getElementById("jsContentToggler").addEventListener("click", function () {
+  document.getElementById("jsContent").style.display = "block"; 
+  document.getElementById("jsContentToggler").innerText = "JS"
 });
